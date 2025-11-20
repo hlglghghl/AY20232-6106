043ddd47266925bbaaff730a5aac51750731cd29
hlglghghl <18515530801@163.com>
2025-11-20 12:12:27 +0000

    1

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    q=request.form.get("q")
+    return render_template("main.html",query="q")
 if __name__=="__main__":
-    app.run()
+    app.run(port=5001)
